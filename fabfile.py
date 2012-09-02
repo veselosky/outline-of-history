09247dab40c0a3d6582784d2618996491f594c24
Vince Veselosky <vince@control-escape.com>
2012-09-02 12:06:04 -0400

    upgrade to otto 0.4

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,10 @@
 from fabric.api import env, lcd, local, task
-import os.path
 import otto.web as web
+from otto.util import paths
 
-env['otto.web.build_dir'] = './build/staged'
-env['otto.web.site'] = 'outline-of-history.mindvessel.net'
-
-def _project_dir():
-    """Helper to return the project directory"""
-    return os.path.dirname(os.path.realpath(env['real_fabfile']))
+env['otto.build_dir'] = 'build/staged'
+env['otto.site'] = 'outline-of-history.mindvessel.net'
+env['otto.home'] = '/home/vince/websites'
 
 @task
 def B():
@@ -17,14 +14,15 @@ def B():
 @task
 def build():
     """Build the site"""
-    target = env['otto.web.build_dir']
-    with lcd(_project_dir()):
+    target = env['otto.build_dir']
+    with lcd(paths.local_workspace()):
         local('sphinx-build -n -d build/doctrees -b html source %s/htdocs' % target)
         local('cp -a etc %s/' % target)
 
 @task
 def clean():
     """Clean up stale build files and tarballs"""
-    with lcd(_project_dir()):
-        local('rm -rf build/*') # make clean
-        local('rm -f *.tar.gz') # make clean
+    with lcd(paths.local_workspace()):
+        local('rm -rf build/*')
+        local('rm -f *.tar.gz')
+        local("find . -name '*.py[co]' | xargs rm")
